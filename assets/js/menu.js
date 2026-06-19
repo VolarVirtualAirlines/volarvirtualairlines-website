6f6c6e980d3dd5298a43fb20c9a0e24f456dc1fe
Volar Virtual Airlines <volarvirtualairlines@gmail.com>
2026-06-19 15:00:50 -0300

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -67,4 +67,31 @@ menuContainer.innerHTML = `
 </nav>
 `;
 
+    document.querySelectorAll('.menu a[href*="#"]').forEach(link => {
+    link.addEventListener("click", function (event) {
+        const href = this.getAttribute("href");
+
+        if (!href.includes("#")) return;
+
+        const [pagina, idSecao] = href.split("#");
+
+        const paginaAtual = window.location.pathname.split("/").pop() || "index.html";
+
+        if (pagina && pagina !== paginaAtual) return;
+
+        const alvo = document.getElementById(idSecao);
+
+        if (!alvo) return;
+
+        event.preventDefault();
+
+        const alturaMenu = document.querySelector(".navbar").offsetHeight;
+        const posicaoAlvo = alvo.getBoundingClientRect().top + window.scrollY - alturaMenu;
+
+        window.scrollTo({
+            top: posicaoAlvo,
+            behavior: "smooth"
+        });
+    });
+});
 });
