94a8211bff938d16c47992c11554b915c0eef22d
Volar Virtual Airlines <volarvirtualairlines@gmail.com>
2026-06-19 14:41:10 -0300

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -4,35 +4,67 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (!menuContainer) return;
 
-    menuContainer.innerHTML = `
-        <nav class="menu">
+menuContainer.innerHTML = `
+<nav class="menu">
 
-            <a href="index.html">Home</a>
+    <a href="index.html">Home</a>
 
+    <div class="menu-dropdown">
+        <a href="#" class="menu-dropdown-toggle">
+            Volar <i class="fas fa-chevron-down"></i>
+        </a>
+
+        <div class="menu-dropdown-content">
             <a href="index.html#sobre-nos">Sobre Nós</a>
-
-            <a href="cargo.html">Volar Cargo</a>
-
-            <a href="frota.html">Frota</a>
-
-            <a href="rotas.html">Rotas</a>
-
-            <a href="index.html#mapa">Online</a>
-
+            <a href="index.html#simuladores">Plataformas</a>
             <a href="news.html">Volar News</a>
+        </div>
+    </div>
 
+    <div class="menu-dropdown">
+        <a href="#" class="menu-dropdown-toggle">
+            Operações <i class="fas fa-chevron-down"></i>
+        </a>
+
+        <div class="menu-dropdown-content">
+            <a href="rotas.html">Rotas Oficiais</a>
+            <a href="index.html#mapa">Mapa Online</a>
+            <a href="index.html#voos-reais">Voos em Tempo Real</a>
+            <a href="index.html#voos-recentes">Voos Recentes</a>
+        </div>
+    </div>
+
+    <div class="menu-dropdown">
+        <a href="#" class="menu-dropdown-toggle">
+            Frota <i class="fas fa-chevron-down"></i>
+        </a>
+
+        <div class="menu-dropdown-content">
+            <a href="frota.html">Frota Volar</a>
+            <a href="cargo.html">Volar Cargo</a>
+        </div>
+    </div>
+
+    <div class="menu-dropdown">
+        <a href="#" class="menu-dropdown-toggle">
+            Comunidade <i class="fas fa-chevron-down"></i>
+        </a>
+
+        <div class="menu-dropdown-content">
+            <a href="index.html#social">Redes Sociais</a>
             <a href="index.html#parceiros">Parceiros</a>
+            <a href="index.html#links">Links Úteis</a>
+        </div>
+    </div>
 
-            <a href="index.html#links">Links</a>
+    <a href="https://newsky.app/pilot/login"
+       target="_blank"
+       rel="noopener noreferrer"
+       class="btn-va">
+       Crew Room
+    </a>
 
-            <a href="https://newsky.app/pilot/login"
-               target="_blank"
-               rel="noopener noreferrer"
-               class="btn-va">
-               Crew Room
-            </a>
-
-        </nav>
-    `;
+</nav>
+`;
 
 });
